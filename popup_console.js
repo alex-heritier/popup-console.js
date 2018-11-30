383fdaf2c35deff7c8ec80d67e00f078f231e8db
alex-heritier <alex.heritier@gmail.com>
2018-11-29 19:04:56 -0800

    console can now be toggled on/off

--- popup_console.js
+++ popup_console.js
@@ -1,8 +1,48 @@
 
 'use strict';
 
-export default Class PopupConsole {
+export default class PopupConsole {
+
   constructor(config) {
-    console.log(config);
+    this.config = config;
+    console.log("config", config);
+  }
+
+  toggle() {
+    if (this.isVisible())   this.hide();
+    else                    this.show();
+  }
+
+  isVisible() {
+    return $('#pcon-window').length > 0;
+  }
+
+  hide() {
+    console.log("Hiding");
+    $('#pcon-window').remove();
+  }
+
+  show() {
+    console.log("Showing");
+    const conWindow = this.createWindow();
+    $(document.body).append(conWindow);
+  }
+
+  createWindow() {
+    const conWindow = $('<div id="pcon-window">');
+    const conBackground = $('<div id="pcon-background">');
+    const conConsole = $('<div id="pcon-console">');
+    const conHistory = $('<div id="pcon-history">');
+    const conInput = $('<div id="pcon-input">');
+
+    conConsole.append(conHistory);
+    conConsole.append(conInput);
+
+    conWindow.append(conBackground);
+    conWindow.append(conConsole);
+
+    $(document.body).append(conWindow);
+
+    conBackground.click(this.hide);
   }
 }
